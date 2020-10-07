f051312a3af925440e92b42d148f90cb9aab0d82
jashan <jashandhanoa13@gmail.com>
2020-10-07 21:53:17 +0530

    remove node

--- routes/country.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var Country = require('../controllers/countryController');
-/* GET users listing. */
-router.get('/list', Country.list);
-
-module.exports = router;
